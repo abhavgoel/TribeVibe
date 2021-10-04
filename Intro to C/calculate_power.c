cc32b66c8c1ee0b6ffd6f5692f69d230b803530c
Hrushikesh Rout <hrushikeshrout2002@gmail.com>
2021-10-04 16:18:13 +0500

    calculate_power Added

--- Intro to C/calculate_power.c
+++ Intro to C/calculate_power.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 int main()
 {
-  //your code here
+   int m, n;
+    long long int val = 1;
+    printf("Enter the number you want to evaluate the power\n");
+    scanf("%d", &m);
+    printf("Enter the power of that number\n");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        val = val * m;
+    }
+    printf("The value of %d to the power %d is: %d", m, n, val);
+    
+    return 0;
 }
