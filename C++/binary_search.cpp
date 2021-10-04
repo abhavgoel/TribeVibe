87a0ec7be9fda0e49546854ed45b0fd9527cb207
abhaybedi <75904669+abhaybedi@users.noreply.github.com>
2021-10-04 21:26:26 +0530

    Update binary_search.cpp

--- C++/binary_search.cpp
+++ C++/binary_search.cpp
@@ -1,6 +1,26 @@
-#include<bits/stdc++.h>
+# include<iostream>
 using namespace std;
+    
+
 int main()
 {
-  //your code here
+        int a[10]={6,8,13,17,20,22,25,28,30,35};
+ 	int l=0,h=9,key,mid;
+	cout<<"enter the key;
+	cin>>key;
+	whlie(l<=h)
+	{
+		mid=(l+h)/2;
+		if(key==a[mid])
+		{
+			cout<<"found at"<<mid;
+			return 0;
+		}
+		else if(key<a[mid])
+			h=mid-l;
+		else
+			l=mid+l;
+	}
+	cout<<"not found";
+	return 0;
 }
