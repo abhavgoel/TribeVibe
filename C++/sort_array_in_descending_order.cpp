64647be6fbfce40ec9f205b9ce108908e4d4b85f
WrathOP <72385500+WrathOP@users.noreply.github.com>
2021-10-05 16:09:36 +0530

    Update sort_array_in_descending_order.cpp

--- C++/sort_array_in_descending_order.cpp
+++ C++/sort_array_in_descending_order.cpp
@@ -1,6 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+  
 int main()
 {
-  //your code here
+    int arr[] = { 1, 45, 54, 71, 76, 12 };
+  
+   
+    int n = sizeof(arr) / sizeof(arr[0]);
+  
+
+    cout << "Array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+  
+
+    sort(arr, arr + n, greater<int>());
+
+    cout << "\nDescending Sorted Array:\n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
 }
