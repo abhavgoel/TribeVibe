7030110a368549eff4a36b4a9a8fcab8bb189066
aparna667 <74860766+aparna667@users.noreply.github.com>
2021-10-06 01:21:14 +0530

    Update insert_element_in _array.cpp

--- C++/insert_element_in _array.cpp
+++ C++/insert_element_in _array.cpp
@@ -1,6 +1,53 @@
-#include<bits/stdc++.h>
+
+#include <iostream>
 using namespace std;
+
+int* insertX(int n, int arr[],
+			int x, int pos)
+{
+	int i;
+
+	
+	n++;
+
+	
+	for (i = n; i >= pos; i--)
+		arr[i] = arr[i - 1];
+
+	
+	arr[pos - 1] = x;
+
+	return arr;
+}
+
+
 int main()
 {
-  //your code here
+	int arr[100] = { 0 };
+	int i, x, pos, n = 10;
+
+	
+	for (i = 0; i < 10; i++)
+		arr[i] = i + 1;
+
+	
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+
+
+	x = 50;
+
+	
+	pos = 5;
+
+
+	insertX(n, arr, x, pos);
+
+
+	for (i = 0; i < n + 1; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+
+	return 0;
 }
