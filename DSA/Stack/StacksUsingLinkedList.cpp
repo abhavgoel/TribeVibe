7e57723df0d107e046cca4ae8ffa11a3146e56c1
Ayush Nagpure <80613051+Jubbu05@users.noreply.github.com>
2021-10-05 15:42:20 +0530

    Create StacksUsingLinkedList.cpp

---
+++ DSA/Stack/StacksUsingLinkedList.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class Node{
+    public:
+    T data;
+    Node<T> *next;
+
+    Node(T data){
+        this->data = data;
+        next = NULL;
+    }
+};
+
+template <typename T>
+class Stack{
+    Node<T> *head;
+    int size;
+
+    public:
+
+    Stack(){
+        head = NULL;
+        size = 0;
+    }
+
+    int getSize(){
+        return size;
+    }
+
+    bool isEmpty(){
+        return size == 0;
+    }
+
+    void push(T element){
+        Node<T> *newNode = new Node<T>(element);
+        newNode->next = head;
+        head = newNode;
+        size++;
+    }
+
+    T pop(){
+        if(isEmpty()){
+            return 0;
+        }
+        T ans = head->data;
+        Node<T> *temp = head;
+        head = head->next;
+        delete temp;
+        size--;
+        return ans;
+    }
+
+    T top(){if(isEmpty()){
+            return 0;
+        }
+        return head->data;
+    }
+
+};
+
+// int main(){
+//     Stack<int> s;
+//     s.push(101);
+//     s.push(102);
+//     s.push(103);
+//     s.push(104);
+//     s.push(105);
+
+//     cout<<s.top()<<endl;
+//     cout<<s.pop()<<endl;
+//     cout<<s.pop()<<endl;
+//     cout<<s.pop()<<endl;
+//     cout<<s.getSize()<<endl;
+//     cout<<s.isEmpty()<<endl;
+// }
