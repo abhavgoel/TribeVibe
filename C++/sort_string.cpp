2f242caea32d356adfb4dab12750f92c58597f87
WrathOP <72385500+WrathOP@users.noreply.github.com>
2021-10-05 23:31:07 +0530

    Update sort_string.cpp

--- C++/sort_string.cpp
+++ C++/sort_string.cpp
@@ -1,6 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+ 
+const int MAX_CHAR = 26;
+
+void sortString(string &str)
+{
+    
+    int charCount[MAX_CHAR] = {0};
+
+    for (int i=0; i<str.length(); i++)
+ 
+       
+        charCount[str[i]-'a']++;   
+     
+
+    for (int i=0;i<MAX_CHAR;i++)
+        for (int j=0;j<charCount[i];j++)
+            cout << (char)('a'+i);
+}
+ 
+// Driver program
 int main()
 {
-  //your code here
+    string s = "geeksforgeeks";   
+    sortString(s);   
+    return 0;
 }
