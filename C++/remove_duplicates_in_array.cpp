5f417431d8fe0d33a7cbeee04f972ad25fc50cc1
aparna667 <74860766+aparna667@users.noreply.github.com>
2021-10-05 23:35:59 +0530

    Update remove_duplicates_in_array.cpp

--- C++/remove_duplicates_in_array.cpp
+++ C++/remove_duplicates_in_array.cpp
@@ -1,6 +1,38 @@
-#include<bits/stdc++.h>
+
+#include<iostream>
 using namespace std;
+
+
+int removeDuplicates(int arr[], int n)
+{
+	if (n==0 || n==1)
+		return n;
+
+	
+	int j = 0;
+
+	
+	for (int i=0; i < n-1; i++)
+		if (arr[i] != arr[i+1])
+			arr[j++] = arr[i];
+
+	arr[j++] = arr[n-1];
+
+	return j;
+}
+
+
 int main()
 {
-  //your code here
+	int arr[] = {1, 1, 2, 3, 3, 3, 4, 4};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	
+	n = removeDuplicates(arr, n);
+
+	
+	for (int i=0; i<n; i++)
+		cout << arr[i] << " ";
+
+	return 0;
 }
