fee9b2751f4dad96dcff9a8632cbd2721c12d9f1
Gyan-Singh <92247410+Gyan-Singh@users.noreply.github.com>
2021-10-11 22:56:10 +0530

    Update reverse.py

--- Python/reverse.py
+++ Python/reverse.py
@@ -1,8 +1,9 @@
-num = input("enter your number")
+
+num = input("enter your number")                          #Taking number from user
 reversed_num = 0
 
-while num != 0:
-    digit = num % 10
+while num != 0:                                            #while loop is used here
+    digit = num % 10                                       # '%' operator finds modulus(remainder)  
     reversed_num = reversed_num * 10 + digit
     num //= 10
 
