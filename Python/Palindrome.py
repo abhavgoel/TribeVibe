9ff455a2552b9858ab36a5d9ccb3b20aa6740e35
Yashasvi-b <92221403+Yashasvi-b@users.noreply.github.com>
2021-10-12 23:00:48 +0530

    Update Palindrome.py

--- Python/Palindrome.py
+++ Python/Palindrome.py
@@ -1,12 +1,7 @@
+number = input('enter a  number')
 
-n=int(input("Enter number:"))                                  #Take number from user 
-temp=n                                                               
-rev=0
-while(n>0):                                                     #while loop is used
-    dig=n%10                                                    # '%' operator find the modulus(remainder) of 2 diving number
-    rev=rev*10+dig
-    n=n//10
-if(temp==rev):                                                  #checking entered number is palindrome or not
-    print("The number is a palindrome!")
+if number == number[::-1]:
+    print("it is a palindrome!") 
+
 else:
-    print("The number isn't a palindrome!")
+    print("it is not a palindrome!") 
