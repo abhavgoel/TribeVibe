af10c9858e4dd2d31f856f8b11f6427feff6d291
Yashasvi-b <92221403+Yashasvi-b@users.noreply.github.com>
2021-10-12 22:57:23 +0530

    Update Fibonacci.py

--- Python/Fibonacci.py
+++ Python/Fibonacci.py
@@ -1,21 +1,31 @@
-
-n = int(input("How many terms? "))
-n1, n2 = 0, 1
-n = int(input("How many terms? "))                                  #Taking number from user
-n1, n2 = 0, 1                                                       #defining variables 
-c = 0
-if n <= 0:
-if n <= 0:                                                          #checking number is negative number or not
-   print("Please enter a positive integer")
-elif n == 1:
-elif n == 1:                                                        #checking number is equal to 1 or not
-   print("Fibonacci sequence upto",n,":")
-   print(n1)
-else:
-else:                                                               
-   print("Fibonacci sequence:")
-   while c < n:
-   while c < n:                                                     #while loop is used to done this program
-       print(n1)
-       nth = n1 + n2
-       n1 = n2
+# Function for nth fibonacci
+# number - Space Optimisataion
+# Taking 1st two fibonacci numbers as 0 and 1
+ 
+def fibonacci(n):
+    a = 0
+    b = 1
+     
+    # Check is n is less
+    # than 0
+    if n < 0:
+        print("Incorrect input")
+         
+    # Check is n is equal
+    # to 0
+    elif n == 0:
+        return 0
+       
+    # Check if n is equal to 1
+    elif n == 1:
+        return b
+    else:
+        for i in range(1, n):
+            c = a + b
+            a = b
+            b = c
+        return b
+ 
+# Driver Program
+n=int(input('enter n'))
+print(fibonacci(n))
