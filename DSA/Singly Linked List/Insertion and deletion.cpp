9d9f7a59c704f86ec0d8aebea1d2bce1c475c9d7
ASTHA AGARWAL <86525559+asthaagarwal01@users.noreply.github.com>
2021-10-17 16:37:45 +0530

    Update and rename Insertion.cpp to Insertion and deletion.cpp

--- DSA/Singly Linked List/Insertion and deletion.cpp
+++ DSA/Singly Linked List/Insertion and deletion.cpp
@@ -69,6 +69,54 @@ void insertpos(int d,int p)
     }
 }
 
+void deletebeg()
+{
+    node *temp=new node;
+    if(head==NULL)
+        cout<<"list is already empty"<<endl;
+    else
+    {
+        temp=head;
+        head=head->next;
+        delete temp;
+    }
+}
+
+void deleteend()
+{
+    node *temp,*cur;
+    temp=head;
+    while(temp->next!=NULL)
+    {
+        cur=temp;
+        temp=temp->next;
+    }
+    cur->next=NULL;
+    delete temp;
+}
+
+void deletepos(int p)
+{
+    node *cur,*pre;
+    cur=head;
+    if(p<=1)
+    {
+        cur=head;
+        head=head->next;
+        delete cur;
+    }
+    else
+    {
+        while(--p && cur->next!=NULL)
+        {
+            pre=cur;
+            cur=cur->next;
+        }
+        pre->next=cur->next;
+        delete cur;
+    }
+}
+
 void display()
 {
     node *temp;
@@ -92,8 +140,11 @@ int main()
     cout<<endl<<"1. Insert at the beginning ."<<endl;
     cout<<"2. Insert at the end ."<<endl;
     cout<<"3. Insert at the given position ."<<endl;
-    cout<<"4. Display the list ."<<endl;
-    cout<<"5. To exit ."<<endl;
+    cout<<"4. Delete at the beginning ."<<endl;
+    cout<<"5. Delete at the end ."<<endl;
+    cout<<"6. Delete at the given position ."<<endl;
+    cout<<"7. Display the list ."<<endl;
+    cout<<"8. To exit ."<<endl;
     cout<<endl<<"Enter your choice : ";
     cin>>c;
     switch(c)
@@ -118,12 +169,28 @@ int main()
             insertpos(e,pos);
             break;
 
-            case 5:
-            exit(0);
-
             case 4:
+            deletebeg();
+            break;
+
+            case 5:
+            deleteend();
+            break;
+
+            case 6:
+            cout<<"Enter the position : ";
+            cin>>pos;
+            deletepos(pos);
+            break;
+
+            case 7:
             display();
             break;
+
+            case 8:
+            exit(0);
+
+
 
             default:
             cout<<"Wrong choice entered , enter again ."<<endl;
