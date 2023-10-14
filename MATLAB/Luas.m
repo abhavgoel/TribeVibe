9cfbc539f525a851093071b84c48ce8fbd8ff9bd
Hasbi Setiawan <64304827+hasbisetiawan@users.noreply.github.com>
2023-10-14 09:22:31 +0700

    Add files via upload

---
+++ MATLAB/Luas.m
@@ -0,0 +1,12 @@
+clear all;
+clc;
+%------------------------------------
+%Program LATIHAN
+%------------------------------------
+disp('/------------------------------------|');
+disp('\           PROGRAM LATIHAN         ||');
+disp(' \-----------------------------------|');
+panjang=input('Masukan Panjangnya :');
+lebar=input('Masukan Lebarnya :');
+luas=panjang*lebar;
+disp(['Luasnya adalah = ' num2str(luas)]);
